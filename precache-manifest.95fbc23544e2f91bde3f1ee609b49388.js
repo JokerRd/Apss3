cedf58d2599cab6c8ccce875fdb4d9fa2be088a7
JokerRd <56046990+JokerRd@users.noreply.github.com>
2020-05-19 20:43:17 +0500

    Updates

---
+++ precache-manifest.95fbc23544e2f91bde3f1ee609b49388.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83c27e9c1066f5d3d4af1c5a5314387",
+    "url": "/Apss3/index.html"
+  },
+  {
+    "revision": "d162c078f45cd32da122",
+    "url": "/Apss3/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "dabe607a707a80b1f1f0",
+    "url": "/Apss3/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d162c078f45cd32da122",
+    "url": "/Apss3/static/js/2.71bfb2a3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/Apss3/static/js/2.71bfb2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabe607a707a80b1f1f0",
+    "url": "/Apss3/static/js/main.306b842d.chunk.js"
+  },
+  {
+    "revision": "e39a983f88d24d57afd6",
+    "url": "/Apss3/static/js/runtime-main.0a1773a6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Apss3/static/media/persik.4e1ec840.png"
+  }
+]);
